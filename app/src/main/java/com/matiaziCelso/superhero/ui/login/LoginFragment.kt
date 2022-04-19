477f3fbce71d3a186d6ef867dfe8796badd41d70
CelsoMatiazi <celsomatiazi@gmail.com>
2022-04-19 10:32:34 -0300

    login com email e senha pelo firebase Auth

--- app/src/main/java/com/matiaziCelso/superhero/ui/login/LoginFragment.kt
+++ app/src/main/java/com/matiaziCelso/superhero/ui/login/LoginFragment.kt
@@ -1,23 +1,34 @@
 package com.matiaziCelso.superhero.ui.login
 
-import android.content.Intent
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 import com.matiaziCelso.superhero.R
-import com.matiaziCelso.superhero.ui.home.HomeActivity
-import com.matiaziCelso.superhero.ui.loadingScreen.LoadingActivity
+import com.matiaziCelso.superhero.data.FavItems
+import com.matiaziCelso.superhero.data.models.ComicItem
 
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: TextInputEditText
+    private lateinit var password: TextInputEditText
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        auth = FirebaseAuth.getInstance()
+
         val register : TextView = view.findViewById(R.id.txt_cadastrar)
         val login : TextView = view.findViewById(R.id.login_entrar_btn)
+        email = view.findViewById(R.id.login_email_txt)
+        password = view.findViewById(R.id.login_pass_txt)
 
         register.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()
@@ -27,11 +38,38 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         }
 
         login.setOnClickListener(){
-            val intent = Intent(it.context, HomeActivity::class.java)
+            loginWithEmailAndPassword()
 
-            startActivity(intent)
+
+//            val intent = Intent(it.context, HomeActivity::class.java)
+//            startActivity(intent)
         }
 
+    }
+
+
+    private fun loginWithEmailAndPassword(){
+        auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    Toast.makeText(requireContext(), "Login success", Toast.LENGTH_LONG).show()
+                }else{
+                    showDialog()
+                    //Toast.makeText(requireContext(), "Falha no Login", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+
+    private fun showDialog(){
+        val alertDialog = AlertDialog.Builder(context, R.style.AppCompatAlertDialogStyle)
+        alertDialog
+            .setTitle("Super Hero")
+            .setMessage("Desculpe, não conseguimos efetuar o login, verifique seu email e senha e tente novamente.")
+            .setCancelable(false)
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
 
--- app/src/main/java/com/matiaziCelso/superhero/ui/login/RegisterFragment.kt
+++ app/src/main/java/com/matiaziCelso/superhero/ui/login/RegisterFragment.kt
@@ -1,23 +1,90 @@
 package com.matiaziCelso.superhero.ui.login
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 import com.matiaziCelso.superhero.R
 
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
 
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: TextInputEditText
+    private lateinit var pass1: TextInputEditText
+    private lateinit var pass2: TextInputEditText
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+
         val returnLogin: TextView = view.findViewById(R.id.return_login)
+        val registerBtn: Button = view.findViewById(R.id.cadastrar_btn)
+        email = view.findViewById(R.id.user_email_txt)
+        pass1 = view.findViewById(R.id.user_pass1)
+        pass2 = view.findViewById(R.id.user_pass2)
 
         returnLogin.setOnClickListener {
            activity?.onBackPressed()
         }
+
+        registerBtn.setOnClickListener {
+            validate()
+        }
+
+    }
+
+
+    private fun validate(){
+        if(email.text!!.contains("@").not() || email.text!!.contains(".").not()){
+            Toast.makeText(requireContext(), "Email invalido", Toast.LENGTH_LONG).show()
+        }else{
+            if(pass1.text?.isEmpty()!!){
+                Toast.makeText(requireContext(), "Senha invalida", Toast.LENGTH_LONG).show()
+            }else{
+                if(pass1.text?.length!! < 6){
+                    Toast.makeText(requireContext(), "Senha muito curta", Toast.LENGTH_LONG).show()
+                }else{
+                    if(pass1.text.toString() != pass2.text.toString()){
+                        Toast.makeText(requireContext(), "Senhas não conferem", Toast.LENGTH_LONG).show()
+                    }else{
+                        createNewAccount()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun createNewAccount(){
+        auth.createUserWithEmailAndPassword(email.text.toString(), pass1.text.toString())
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    Toast.makeText(requireContext(), "Login success", Toast.LENGTH_LONG).show()
+                }else{
+                    showDialog(it.exception?.message.toString())
+
+                    //Toast.makeText(requireContext(), "Falha no Login", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+
+    private fun showDialog(message: String){
+        val alertDialog = AlertDialog.Builder(context, R.style.AppCompatAlertDialogStyle)
+        alertDialog
+            .setTitle("Super Hero")
+            .setMessage("Desculpe, não conseguimos efetuar seu cadastro.\n\n\"$message\"")
+            .setCancelable(false)
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
 
