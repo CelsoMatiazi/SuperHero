505272abf3a7f8698808d0aaec914e3cdeb34347
CelsoMatiazi <celsomatiazi@gmail.com>
2022-04-19 11:53:56 -0300

    ajustando o codigo

--- app/src/main/java/com/matiaziCelso/superhero/ui/login/LoginFragment.kt
+++ app/src/main/java/com/matiaziCelso/superhero/ui/login/LoginFragment.kt
@@ -3,12 +3,10 @@ package com.matiaziCelso.superhero.ui.login
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.os.Message
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.view.isVisible
 import com.airbnb.lottie.LottieAnimationView
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -19,8 +17,6 @@ import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.matiaziCelso.superhero.R
-import com.matiaziCelso.superhero.data.FavItems
-import com.matiaziCelso.superhero.data.models.ComicItem
 import com.matiaziCelso.superhero.ui.home.HomeActivity
 import com.matiaziCelso.superhero.utils.GoogleLogInActivityContract
 
